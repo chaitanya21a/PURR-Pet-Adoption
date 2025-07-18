aac6f2bf0b21a370015027c88d6adb5aabb93936
chaitanya21a <tanya242005@gmail.com>
2025-07-18 23:45:30 +0530

    fixes

--- backend/controller/AdminController.js
+++ backend/controller/AdminController.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const getCredentials = (req, res) => {
-    const Credentials = { "username": "admin", "password": "123" }
+    const Credentials = { "username": "admin", "password": "123456" }
     res.status(200).json(Credentials)
 }
 
