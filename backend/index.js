12ceca77e5c20aea14ca7c30e9de4b1b905f6f23
chaitanya21a <tanya242005@gmail.com>
2025-08-02 17:23:21 +0530

    minor fixes

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const petRouter = require('./Routes/PetRoute')
-const AdoptFormRoute = require('./Routes/AdoptFormRoute')
-const AdminRoute = require('./Routes/AdminRoute')
+const petRouter = require('./Routes/PetRoute.js')
+const AdoptFormRoute = require('./Routes/AdoptFormRoute.js')
+const AdminRoute = require('./Routes/AdminRoute.js')
 const cors = require('cors');
 
 const app = express();
